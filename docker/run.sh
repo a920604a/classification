17ea39100319d684752b7964b6e1e4c55878af28
Chen yuan <a920604a@gmail.com>
2021-03-10 18:03:21 +0800

    v2

--- docker/run.sh
+++ docker/run.sh
@@ -2,8 +2,8 @@
 ###
 # @Author: yuan
 # @Date: 2021-02-22 09:33:58
- # @LastEditTime: 2021-03-10 13:16:59
- # @FilePath: /yuan-algorithm/docker/run.sh
+ # @LastEditTime: 2021-03-10 18:02:51
+ # @FilePath: /classification/docker/run.sh
 ###
 sudo docker-compose up -d
 # sudo docker-compose run --name yuan yuan-algorithm bash
